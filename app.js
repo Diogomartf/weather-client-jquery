6de3df29675fe75ff518b5fb823a24a0f4345957
Diogo Ferreira <diogo.mart.ferreira@gmail.com>
2015-11-08 01:40:33 +0000

    Added 5 days forecast with icons by weather

--- app.js
+++ app.js
@@ -1,15 +1,55 @@
 
 /*ao carregar no botao submeter vai buscar o valor que esta lá e chama a funcao que faz o pedido para ir buscar o tempo,
  depois muda a propriadade display para mostrar os elementos que estao escondidos*/
-$(document).on("click", ":submit", function(e){
-    var city_name = $('#city_name').val();
-    getWeather(city_name);
+
+function getForecast(city_name) {
+    $('#forecast').css({
+        display: 'block'
+    });
+    
+    $.ajax("http://api.openweathermap.org/data/2.5/forecast?",
+    {   
+        method: "GET",
+        dataType : "json" ,
+        data: {
+          APPID: "105c6a96fc6cc478bc7c239d696a4d04",
+          q: city_name,
+          units: "metric"
+        },
+        success: function(response) {
+            document.getElementById("day1").innerHTML = response['list'][0].main.temp + " ºC";
+            document.getElementById("day2").innerHTML = response['list'][5].main.temp + " ºC";
+            document.getElementById("day3").innerHTML = response['list'][10].main.temp + " ºC";
+            document.getElementById("day4").innerHTML = response['list'][15].main.temp + " ºC";
+            document.getElementById("day5").innerHTML = response['list'][20].main.temp + " ºC";
+            document.getElementById("humidity1").innerHTML = response['list'][0].main.humidity;
+            document.getElementById("humidity2").innerHTML = response['list'][5].main.humidity;
+            document.getElementById("humidity3").innerHTML = response['list'][10].main.humidity;
+            document.getElementById("humidity4").innerHTML = response['list'][15].main.humidity;
+            document.getElementById("humidity5").innerHTML = response['list'][20].main.humidity;       
+            document.getElementById("description1").innerHTML = response['list'][0].weather[0].description;    
+            document.getElementById("description2").innerHTML = response['list'][5].weather[0].description;
+            document.getElementById("description3").innerHTML = response['list'][10].weather[0].description;
+            document.getElementById("description4").innerHTML = response['list'][15].weather[0].description;
+            document.getElementById("description5").innerHTML = response['list'][20].weather[0].description;
+            var cont = 0;
+            for(var i = 0; i <= 20; i += 5){
+              cont++;
+             whatIcon(response['list'][i].weather[0].description, cont);
+            }
+        },
+        error: function(xhr) {
+           alert("Erro");
+        }
+    });
+
+
+};
+
+function getWeather(city_name) {
     $('#weather').css({
         display: 'block'
     });
-});
-
-function getWeather(city_name) {
     //pedido http get para ir buscar a temperatura od se passa a cidade atraves da variavel city_name, as units: metric é para vir em celcius, appid é api key
     $.ajax("http://api.openweathermap.org/data/2.5/weather?",
     {   
@@ -32,6 +72,22 @@ function getWeather(city_name) {
            alert("Erro");
         }
     });
+}
+
+function whatIcon(description, i){
+  if(description.indexOf("clear") > -1){
+      $("#icon".concat(i)).append('<i class="wi wi-day-sunny"></i>');
+  }else if(description.indexOf("light rain") > -1){
+      $("#icon".concat(i)).append('<i class="wi wi-day-rain"></i>');
+  }else if(description.indexOf("few clouds") > -1){
+      $("#icon".concat(i)).append('<i class="wi wi-day-cloudy"></i>');
+  }else if(description.indexOf("scattered ") > -1){
+      $("#icon".concat(i)).append('<i class="wi wi-cloudy"></i>');
+  }else if(description.indexOf("moderate rain") > -1){
+      $("#icon".concat(i)).append('<i class="wi wi-showers"></i>');
+  }else if(description.indexOf("overcast ") > -1){
+      $("#icon".concat(i)).append('<i class="wi wi-cloudy-windy"></i>');
+  };
 }
 
 
